59dda9bb79ab5f978a6862bb0078a2cda185a55c
tochiba <c104kn@gmail.com>
2016-03-05 14:46:04 +0900

    Add RecordViewController

---
+++ Ho/ViewController/RecordViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RecordViewController.swift
+//  Ho
+//
+//  Created by 千葉 俊輝 on 2016/03/05.
+//  Copyright © 2016年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+}
