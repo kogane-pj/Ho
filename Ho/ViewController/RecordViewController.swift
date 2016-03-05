8edbd5f859387c6f46169570783a2051857a16d0
tochiba <c104kn@gmail.com>
2016-03-05 15:16:04 +0900

    Add Record UI

--- Ho/ViewController/RecordViewController.swift
+++ Ho/ViewController/RecordViewController.swift
@@ -7,6 +7,35 @@
 //
 
 import UIKit
+import EZAudio
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, EZMicrophoneDelegate {
+    @IBOutlet weak var audioPlot: EZAudioPlot! {
+        didSet {
+            audioPlot.plotType = EZPlotType.Buffer
+            audioPlot.shouldFill = true
+            audioPlot.shouldMirror = true
+        }
+    }
+    
+    private var audioFile: EZAudioFile!
+    private var mic: EZMicrophone! = {
+        let _mic = EZMicrophone.sharedMicrophone()
+        _mic.startFetchingAudio()
+        _mic.device = EZAudioDevice.inputDevices().last as! EZAudioDevice!
+        return _mic
+    }()
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mic.delegate = self
+    }
+    
+    func microphone(microphone: EZMicrophone!, hasAudioReceived buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
+        
+        dispatch_async(dispatch_get_main_queue(), {() -> Void in
+            self.audioPlot.updateBuffer(buffer[0], withBufferSize: bufferSize)
+        })
+    }
 }
