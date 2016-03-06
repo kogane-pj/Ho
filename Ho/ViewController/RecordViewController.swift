bef41d8377c3477932be5222fe7c1a4546f4e320
tochiba <c104kn@gmail.com>
2016-03-06 23:28:53 +0900

    Add Upload Function And Bug Fix

--- Ho/ViewController/RecordViewController.swift
+++ Ho/ViewController/RecordViewController.swift
@@ -11,7 +11,7 @@ import EZAudio
 import KYShutterButton
 import UNAlertView
 
-class RecordViewController: UIViewController, EZMicrophoneDelegate {
+class RecordViewController: UIViewController, EZMicrophoneDelegate, EZRecorderDelegate {
     @IBOutlet weak var recordButton: KYShutterButton!
     @IBOutlet weak var audioPlot: EZAudioPlot! {
         didSet {
@@ -20,23 +20,35 @@ class RecordViewController: UIViewController, EZMicrophoneDelegate {
             audioPlot.shouldMirror = true
         }
     }
-    
-    private var audioFile: EZAudioFile!
     private var mic: EZMicrophone! = {
         let _mic = EZMicrophone.sharedMicrophone()
         _mic.startFetchingAudio()
         _mic.device = EZAudioDevice.inputDevices().last as! EZAudioDevice!
         return _mic
     }()
+    private var recorder: EZRecorder!
     
     @IBAction func didPushRecordButton(sender: AnyObject) {
         changeRecordState()
     }
     
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AudioManager.sharedInstance.start()
+        self.mic.delegate = self
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
     private func changeRecordState() {
         switch self.recordButton.buttonState {
         case .Normal:
+            setupRecord()
             self.recordButton.buttonState = .Recording
+            
             break
         case .Recording:
             self.recordButton.buttonState = .Normal
@@ -56,24 +68,48 @@ class RecordViewController: UIViewController, EZMicrophoneDelegate {
         alertView.show()
     }
     
-    private func saveRecordData() {
-        // TODO: 録音したファイルをアップロード
+    private func setupRecord() {
+        self.recorder = EZRecorder(URL: getRecordFileURL(), clientFormat: self.mic.audioStreamBasicDescription(), fileType: .AIFF)
+        self.recorder.delegate = self
     }
 
-    // MARK: - Life cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.mic.delegate = self
+    private func saveRecordData() {
+        self.recorder.closeAudioFile()
+        self.recorder.delegate = nil
     }
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .LightContent
+    private func getRecordFileURL() -> NSURL {
+        let dirURL = getDocumentsDirectoryURL()
+        let fileName = "recording.aiff"
+        let recordingsURL = dirURL.URLByAppendingPathComponent(fileName)
+        return recordingsURL
     }
     
+    private func getDocumentsDirectoryURL() -> NSURL {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory,
+            inDomains: NSSearchPathDomainMask.UserDomainMask)
+        
+        if urls.isEmpty {
+            fatalError("URLs for directory are empty.")
+        }
+        return urls[0]
+    }
+    
+    func microphone(microphone: EZMicrophone!, hasBufferList bufferList: UnsafeMutablePointer<AudioBufferList>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
+        if self.recordButton.buttonState == .Recording {
+            self.recorder.appendDataFromBufferList(bufferList, withBufferSize: bufferSize)
+        }
+    }
     func microphone(microphone: EZMicrophone!, hasAudioReceived buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32) {
         
         dispatch_async(dispatch_get_main_queue(), {() -> Void in
             self.audioPlot.updateBuffer(buffer[0], withBufferSize: bufferSize)
         })
+    }
+    
+    // MARK: - EZRecorderDelegate
+    func recorderDidClose(recorder: EZRecorder!) {
+        FileManager.sharedInstance.uploadFile(NSUUID().UUIDString + ".aiff",
+            url: getRecordFileURL())
     }
 }
